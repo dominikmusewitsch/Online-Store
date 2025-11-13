54b5a40371b4e8085e658dfd9bfa3d1e52bb4f5d
dominikmusewitsch <dmusewitsch@gmail.com>
2025-11-13 15:51:33 +0100

    delete jest.config.ts

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/test'],
-};
-
-export default config;
